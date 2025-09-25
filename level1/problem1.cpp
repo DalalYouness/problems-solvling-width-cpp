cc15102c88af9c301b269b8cb40cef2a0f5b99fc
dalal youness <younessdalal1@gmail.com>
2025-09-25 10:53:35 +0100

    initial commit

---
+++ level1/problem1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+void PrintName(string Name)
+{
+    cout << "\nyour Name is " << Name << endl;
+}
+
+int main() {
+
+    PrintName("Youness");
+
+    return 0;
+
+}
